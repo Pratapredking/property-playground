fa111558f47a5e490108b86ca60c0148924dc713
pk <pk@example.com>
2025-04-25 16:29:45 +0530

    changes

--- app/properties/[...id]/page.jsx
+++ app/properties/[...id]/page.jsx
@@ -3,6 +3,7 @@ import Property from '/models/Property';
 import PropertyHeaderImage from '/component/PropertyHeaderImage';
 import Link from 'next/link';
 import { FaArrowLeft } from 'react-icons/fa';
+import PropertyDetails from '@/component/PropertyDetails';
 
 const PropertyPage = async ({ params }) => {
     await connectDB();
@@ -18,16 +19,22 @@ const PropertyPage = async ({ params }) => {
         <>
            <PropertyHeaderImage image={property.images[0]}/>
            <section>
-      <div class="container m-auto py-6 px-6">
+      <div className="container m-auto py-6 px-6">
         <Link
           href="/properties"
-          class="text-blue-500 hover:text-blue-600 flex items-center"
+          className="text-blue-500 hover:text-blue-600 flex items-center"
         >
           <FaArrowLeft className='mr-2'/> Back to Properties
         </Link>
       </div>
     </section>
-          <section> {property.name}</section>
+    <section className="bg-blue-50">
+      <div className="container m-auto py-10 px-6">
+        <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+               <PropertyDetails property={property}/>
+        </div>
+        </div>
+        </section>
 
         </>
     );
